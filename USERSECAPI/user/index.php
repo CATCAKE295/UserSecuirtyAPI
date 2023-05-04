1f8e0d75964cb300a5490b14d29da9dd9d48c015
GhostlyCoder20 <119901915+GhostlyCoder20@users.noreply.github.com>
2023-05-03 19:51:52 -0600

    User Insert and insert in token table

--- USERSECAPI/user/index.php
+++ USERSECAPI/user/index.php
@@ -11,10 +11,30 @@ switch ($_SERVER['REQUEST_METHOD']) {
             echo json_encode(User::getAll());
         }
         break;
-    
-    default:
-        # code...
+
+    case 'POST':
+        $data = json_decode(file_get_contents('php://input'));
+
+        
+        if ($data != NULL) {
+            if (User::insert($data->username,$data->name,$data->lastname,$data->password,$data->email)) {
+                http_response_code(200);
+                echo json_encode(['mensaje' => 'Usuario registrado Correctamente']);
+
+                        
+            } else {
+                http_response_code(400);
+                
+            }
+            
+        } else {
+            http_response_code(405);
+        }
+       
         break;
+
+   
+
 }
 
 
