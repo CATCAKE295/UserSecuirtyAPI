0b5f80d076b98ce52ba39a37dac000741fed613a
GhostlyCoder20 <119901915+GhostlyCoder20@users.noreply.github.com>
2023-05-05 11:45:32 -0600

    User: delete method added and some changes

--- USERSECAPI/user/model/User.php
+++ USERSECAPI/user/model/User.php
@@ -65,19 +65,29 @@ class User{
 
     public static function insert($username,$name,$lastname,$password,$email){
         $db = new Connection();
+        
         $password_hash = password_hash($password, PASSWORD_DEFAULT);
         $query = "INSERT INTO db_user_security.user (username, name, lastname, password, email)
         VALUES ('".$username."','".$name."','".$lastname."','".$password_hash."','".$email."')";
+        
         $db->query($query);
 
         $id = $db->insert_id;
+
+        echo json_encode(['Mensaje' => 'El usuario '. $name .' registrado Correctamente']);
+
+        echo "<br>";
        
     
         if($db->affected_rows){
 
         
             $token = bin2hex(random_bytes(16));
-            $token_cifrado = password_hash($token, PASSWORD_DEFAULT);;
+
+            echo json_encode(['Token Usuario' => 'Token del usuario ' . $token]);
+
+            $token_cifrado = password_hash($token, PASSWORD_DEFAULT);
+
             $queryToken = "INSERT INTO token (id_user, token) VALUES ('".$id."','".$token_cifrado."');";
             $db->query($queryToken);
 
@@ -88,7 +98,22 @@ class User{
         return false;
     }
 
-    
+
+
+    public static function delete($id_user){
+        $db = new Connection();
+
+        $queryUser = "DELETE FROM user WHERE id_user = $id_user;";
+        $queryToken = "DELETE FROM db_user_security.token WHERE id_user = $id_user;";
+
+        if(mysqli_multi_query($db, $queryToken . $queryUser)) {
+            echo json_encode(['mensaje' => 'Las dos consultas se ejecutaron correctamente.']);
+        } else {
+            echo json_encode(['Error' => mysqli_error($db)]);
+        }
+
+        mysqli_close($db);
+    }
 
 
 }
