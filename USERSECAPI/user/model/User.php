053a11c74cd5a98e380830f53902c588d2859d28
GhostlyCoder20 <119901915+GhostlyCoder20@users.noreply.github.com>
2023-05-05 12:09:11 -0600

    User: method update added

--- USERSECAPI/user/model/User.php
+++ USERSECAPI/user/model/User.php
@@ -115,10 +115,21 @@ class User{
         mysqli_close($db);
     }
 
+    public static function update($id_user,$username,$name,$lastname,$password,$email){
+        $db = new Connection();
+        $password_hash = password_hash($password, PASSWORD_DEFAULT);
+        $query = "UPDATE user SET 
+        username= '".$username."', name= '".$name."', lastname='".$lastname."', password='".$password_hash."', email='".$email."' WHERE id_user=$id_user";
+        $db->query($query);
+        if($db->affected_rows){
+            echo json_encode(['Mensaje' => 'El usuario '. $name .' ha sido actualizado correctamente ']);
+            return true;
+        }
+        return false;
+    }
+
 
 }
-
-
 
 
 ?>
